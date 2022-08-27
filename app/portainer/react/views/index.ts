4099e6f056e79998b2a61d01d5c8acb08d097b48
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-08-28 02:55:47 +0300

    refactor(ui): remove QueryProvider

--- app/portainer/react/views/index.ts
+++ app/portainer/react/views/index.ts
@@ -3,10 +3,14 @@ import angular from 'angular';
 import { HomeView } from '@/portainer/home';
 import { withCurrentUser } from '@/portainer/hooks/useUser';
 import { r2a } from '@/react-tools/react2angular';
+import { withReactQuery } from '@/react-tools/withReactQuery';
 
 import { wizardModule } from './wizard';
 import { teamsModule } from './teams';
 
 export const viewsModule = angular
   .module('portainer.app.react.views', [wizardModule, teamsModule])
-  .component('homeView', r2a(withCurrentUser(HomeView), [])).name;
+  .component(
+    'homeView',
+    r2a(withReactQuery(withCurrentUser(HomeView)), [])
+  ).name;
